33a897100c97e2ed041be821f0f6324916ed9532
Wiener234 <70913337+Wiener234@users.noreply.github.com>
2021-12-20 20:29:52 +0100

    adding test file

---
+++ test.cmd
@@ -0,0 +1,10 @@
+pushd %~dp0
+curl -L -o ahk-install.exe https://www.autohotkey.com/download/ahk-install.exe
+curl -L -o Interception.zip https://github.com/oblitum/Interception/releases/download/v1.0.1/Interception.zip
+curl -L -o intercept.zip https://octopup.org/img/code/interception/intercept.zip
+tar -xf Interception.zip
+tar -xf intercept.zip
+ahk-install /S
+cd "%cd%\Interception\command line installer"
+install-interception.exe /install
+pause
